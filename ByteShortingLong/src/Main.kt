cc5eca1bffc08fbeaff8f2163b1d708cc5907619
shrabondas5544 <128902732+shrabondas5544@users.noreply.github.com>
2024-09-02 00:26:39 +0600

    Byte Shorting Long

---
+++ ByteShortingLong/src/Main.kt
@@ -0,0 +1,27 @@
+fun main(){
+    val number: Int = 22
+
+    val maxintegerValue: Int = Int.MAX_VALUE
+    val minintegerValue: Int = Int.MIN_VALUE
+
+    println("Integer Maximun Value is: $maxintegerValue")
+    println("Integer Minimun Value is: $minintegerValue")
+
+    val maxByteValue: Byte = Byte.MAX_VALUE
+    val minByteValue: Byte = Byte.MIN_VALUE
+
+    println("Byte Maximum Value is: $maxByteValue")
+    println("Byte Minimum Value is: $minByteValue")
+
+    val maxShortValue: Short = Short.MAX_VALUE
+    val minShortValue: Short = Short.MIN_VALUE
+
+    println("short Maximum Value is: $maxShortValue")
+    println("short Minimum Value is: $minShortValue")
+
+    val maxLongValue: Long = Long.MAX_VALUE
+    val minLongValue: Long = Long.MIN_VALUE
+
+    println("Long Maximum Value is: $maxLongValue")
+    println("Long Minimum Value is: $minLongValue")
+}
