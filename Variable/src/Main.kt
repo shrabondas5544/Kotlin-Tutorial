c3e8d111db099c46ffd78c2480641723d844835e
shrabondas5544 <128902732+shrabondas5544@users.noreply.github.com>
2024-09-02 00:25:48 +0600

    Variable

---
+++ Variable/src/Main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var userName: String = "shrabon"
+    userName = "Sharbon Das"
+
+    //val userName: String ="Jhon"
+
+    var age : Int = 22
+    age = 20
+    
+    println("hello $userName age is $age")
+}
