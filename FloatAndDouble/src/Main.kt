afe486b5bd819358148cbbfd6616bfe446f7cac8
shrabondas5544 <128902732+shrabondas5544@users.noreply.github.com>
2024-09-02 00:28:39 +0600

    Float and Double

---
+++ FloatAndDouble/src/Main.kt
@@ -0,0 +1,9 @@
+
+fun main() {
+    val myFristNumber = 2.5f  //f after that number stands for Float
+    val mySecondNumber = 2.5 // if we don't declarer anything it will consider as Double
+    val myThirdNumber = 5f
+
+    println("1st,2nd & 3rd num = $myFristNumber,$mySecondNumber & $myThirdNumber")
+
+}
